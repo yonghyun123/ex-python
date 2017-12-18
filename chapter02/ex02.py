8cb283a5f56cc917daf3c5d2bd0455c4e9af5b13
yonghyun123 <model9960@naver.com>
2017-12-18 23:43:08 +0900

    using join method in list

--- chapter02/ex02.py
+++ chapter02/ex02.py
@@ -2,3 +2,12 @@
 items = ['a','b','c'];
 for a in items:
     print(a);
+
+print(items[::-1]);
+
+string = 'abcdefg';
+print(string[::]);
+
+marxes = ['Groucho', 'Chico', 'Harpo'];
+print(','.join(marxes));
+
