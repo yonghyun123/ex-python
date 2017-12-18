f3aa9b41957986cd46bdad4f8557e74096ff678c
yonghyun123 <model9960@naver.com>
2017-12-19 01:34:11 +0900

    solving practice in 3.8

---
+++ chapter02/practice3.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*- 
+
+# 3.1 year_lists
+year_lists = ['1980', '1981', '1982', '1983', '1984']
+
+# 3.2 세 번째 생일
+year_lists[3];
+
+# 3.3 가장 나이가 많은
+year_lists[0];
+
+# 3.4 things
+things = ['mozzarella', 'cinderella', 'salmonella'];
+
+# 3.5 사람 이름의 첫 글짜를 대문짜로 바꿔서 출력
+tempStr = ','.join(things);
+print(tempStr.capitalize())
+
+# 3.6 치즈 요소를 모두 대문자로
+print(tempStr.title());
+
+# 3.8 surprise 리스트
+surprise = ['Groucho', 'Chico', 'Harpo'];
+# 3.9 리스트의 마지막 요소를 소문자로 역전 시킨후, 단어를 역전시킨 후 첫글자를 대문자로 바꿔라
+
